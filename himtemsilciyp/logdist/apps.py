05d78f392e0ab4123a8c7833f71f9c146da91fa1
mdaewx <irmak2259@gmail.com>
2024-07-08 11:00:50 +0300

    first commit

---
+++ himtemsilciyp/logdist/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class LogdistConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'logdist'
---
+++ himtemsilciyp/logdist/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+
+class MyModel(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
